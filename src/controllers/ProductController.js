15e12f4bf142d752b47339532b42557c92389c46
Afonso  Pereira <afonsopereira@Mac-mini-de-Afonso.local>
2020-01-16 17:54:05 -0300

    estudos node

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const product = mongoose.model("Product");
 
 module.exports = {
+  //return all products
   async getall(req, resp) {
     const products = await product.find();
-
     return resp.json(products);
-  }
+  },
+  //insert new product
+  async insert(req, resp) {}
 };
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ const routes = express.Router();
 const productController = require("./controllers/ProductController");
 
 routes.get("/products", productController.getall);
+routes.post("/products", productController.insert);
 
 module.exports = routes;
